0ffdee648735f9d7203bdc5f63e7a4b1c25029c2
Luiz Fernando <luizfernandodematoscarvalho@gmail.com>
2024-03-09 14:13:55 -0300

    aula11 exercises

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel =79.4
+console.log(`Velocidade: ${vel}`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança.')
